d43a0e3399ab1100d70264265ad6ece92ef9b5fa
habai <lijialu0810@163.com>
2024-06-21 15:36:55 +0800

    llllll

--- dvdsb.js
+++ dvdsb.js
@@ -6,4 +6,7 @@
  * @FilePath: \test\dvdsb.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-const a =1;
+const a = 1;
+console.log(a, "a");
+
+const arr = [1, 2, 3, 4];
