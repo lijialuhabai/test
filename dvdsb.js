6ea8399ecd44dca7fb43baf8d3e4e0e04c664a36
jiajugao <2591662204@qq.com>
2024-06-21 15:34:53 +0800

    ggggg

---
+++ dvdsb.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: jiajugao 2591662204@qq.com
+ * @Date: 2024-06-21 15:34:00
+ * @LastEditors: jiajugao 2591662204@qq.com
+ * @LastEditTime: 2024-06-21 15:34:06
+ * @FilePath: \test\dvdsb.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+const a =1;
